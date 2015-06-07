706e5ad50354a0bb714c2cd08811105e7c1c2873
Diana Lozano <Diana@Home-MacBook-Pro.local>
2015-06-07 13:25:29 -0700

    add sinatra crud app files and dependencies

---
+++ db/migrate/01_create_notes.rb
@@ -0,0 +1,10 @@
+class CreateNotes < ActiveRecord::Migration
+  def change
+    create_table :notes do |t|
+      t.string :content, null: false
+      t.boolean :complete, :default => false
+
+      t.timestamps null: false
+    end
+  end
+end
---
+++ recall.rb
@@ -0,0 +1,58 @@
+require 'rubygems'
+require 'sinatra'
+
+require_relative 'config/application'
+
+puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+
+get '/' do
+  @notes = Note.all
+  @title = 'All Notes'
+  erb :home
+end
+
+post '/' do
+  n = Note.new
+  n.content = params[:content]
+  n.created_at = Time.now
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+
+get '/:id' do
+  @note = Note.find(params[:id])
+  @title = "Edit note ##{params[:id]}"
+  erb :edit
+end
+
+put '/:id' do
+  n = Note.find(params[:id])
+  n.content = params[:content]
+  n.complete = params[:complete] ? 1 : 0
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/complete' do
+  n = Note.find(params[:id])
+  n.complete = n.complete ? 0 : 1 # flip it
+  n.updated_at = Time.now
+  n.save
+  redirect '/'
+end
+
+get '/:id/delete' do
+  @note = Note.find(params[:id])
+  @title = "Confirm deletion of note #{params[:id]}"
+  erb :delete
+end
+
+delete '/:id' do
+  n = Note.find(params[:id])
+  n.destroy
+  redirect '/'
+end
